cd85a20142ef1f1a9367a417981c2d7d0fa6b9d9
Thorvald Ballestad <thorvald.tb@gmail.com>
2020-02-05 08:35:34 +0000

    Updates repo with local changes made on server

--- numfys/base.py
+++ numfys/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
+    'sslify.middleware.SSLifyMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- numfys/wsgi.py
+++ numfys/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "numfys.devel")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "numfys.production")
 
 application = get_wsgi_application()
