af2e2a9f28d5c2bf229026923c85ca6c220e1921
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2021-03-20 15:56:08 +0100

    Updates courses file placement

--- course/models.py
+++ course/models.py
@@ -14,12 +14,17 @@ class Course(models.Model):
         ordering = ['-date']
 
 
+
+def course_directory_path(instance, filename):
+    """Return a path for a instance of CourseFile"""
+    return f"course_files/{instance.course.id}/{filename}"
+
 class CourseFile(models.Model):
     # Note: Possible change in future:
     # upload_to with course name in path, this
     # requires Course to handle change in pathname
     # if renaming Course.
-    file = models.FileField(upload_to='course_files')
+    file = models.FileField(upload_to=course_directory_path)
     course = models.ForeignKey("Course",
                                on_delete=models.CASCADE,
                                related_name="file")
